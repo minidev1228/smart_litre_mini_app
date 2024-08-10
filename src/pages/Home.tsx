4b7dd1d804900ec0160a21f68810a66f6c8e2c8f
minidev1228 <mightybluedev@gmail.com>
2024-08-10 10:03:29 -0700

    update_576

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -106,17 +106,6 @@ const HomePage = () => {
 
     
     // const eventBuilder = useTWAEvent();
-
-    useEffect(()=>{
-      const currentDate = new Date();
-
-      // Convert to Dubai time (GMT+4)
-      const options: Intl.DateTimeFormatOptions = { timeZone: 'Asia/Dubai', year: 'numeric', month: '2-digit', day: '2-digit' };
-      const dubaiDate = new Intl.DateTimeFormat('en-US', options).format(currentDate);
-      console.log("========>", dubaiDate.toString());
-      // eventBuilder.track('visit', {});
-    }, [])
-
     const addition = 100 / (10 * (level + 1));
     if (level < 6 && currentLevelProgress <= 100 && energy > 0) {
       setEnergy((prev) => Math.max(prev - 1, 0));
--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -2,6 +2,17 @@ import Logo from "@/assets/svg/logo.svg?react";
 import LogoText from "@/assets/svg/logo-text.svg?react";
 
 const SplashPage = () => {
+  useEffect(()=>{
+    console.log("This is Dubai Now time!");
+    const currentDate = new Date();
+
+    // Convert to Dubai time (GMT+4)
+    const options: Intl.DateTimeFormatOptions = { timeZone: 'Asia/Dubai', year: 'numeric', month: '2-digit', day: '2-digit' };
+    const dubaiDate = new Intl.DateTimeFormat('en-US', options).format(currentDate);
+    console.log("========>", dubaiDate.toString());
+    // eventBuilder.track('visit', {});
+  }, [])
+
   return (
     <div className="flex gap-5 items-center justify-center flex-col h-screen">
       <Logo />
